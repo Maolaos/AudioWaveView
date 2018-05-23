6b692a41e9374e28f73fb11e8a0a43727906e7ab
jiaoyanan <jiao_ya_nan@163.com>
2018-05-23 15:51:14 +0800

    初版

--- app/src/main/java/com/jyy/audiowaveview/MainActivity.java
+++ app/src/main/java/com/jyy/audiowaveview/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
